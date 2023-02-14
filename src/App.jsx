00caa396874901021ad09f7d1ae0e7f2e0580ff2
tiago <tiagoandroloko@gmail.com>
2023-02-14 15:38:38 -0300

    public

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import "./App.css";
+import { fetchWeather } from "./api/fetchApi";
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      console.log(data);
+      setWeather(data);
+      setQuery("");
+    }
+  };
+
+  return (
+    <div className="main-container">
+      <h1>Word Weather Information</h1>
+      
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              className="city-icon"
+            />
+            <p>{weather.weather[0].description}</p>
+            <p>Wind {weather.wind.speed} Km/h</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
